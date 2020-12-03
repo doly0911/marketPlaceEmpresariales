8886db501d6f6380325ae1fa50659c129818fdfd
Sebastian gomez a <sebas970218@gmail.com>
2020-12-03 18:30:56 -0500

    add first api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api1 = axios.create({
-    baseURL: 'api1'
+    baseURL: 'http://blackstorenetcore.eba-py2kgy33.us-east-1.elasticbeanstalk.com/'
 })
 
 const api2 = axios.create({
--- src/services/product.services.js
+++ src/services/product.services.js
@@ -4,40 +4,15 @@ export default {
 
     async getProducts(searchWord) {
 
-        /*const data1 = await api.api1().get(`api/search?q=${searchWord}`)
+        const data1 = await api.api1().get(`api/search?q=${searchWord}`)
+        /*
         const data2 = await api.api2().get(`api/search?q=${searchWord}`)
         const data3 = await api.api3().get(`api/search?q=${searchWord}`)*/
-        const data1 = {
-            query = "",
-            total = 0,
-            seller:
-            {
-                id = "",
-                name = "vendedor 1"
-            },
-            items:
-                [
-                    {
-                        id: ID,
-                        name: "carro vendedor 1 ",
-                        brand: "Oster",
-                        thubnail: "URL",
-                        city:
-                        {
-                            name: "Medellin",
-                            code: "MED"
-                        },
-                        price: 0.0,
-                        currency: "COP",
-                        rating: 0.0
-                    },
 
-                ]
-        }
 
         const data2 = {
-            query = "",
-            total = 0,
+            query = "televisor",
+            total = 1,
             seller:
             {
                 id = "",
@@ -64,8 +39,8 @@ export default {
         }
 
         const data3 = {
-            query = "",
-            total = 0,
+            query = "televisor",
+            total = 1,
             seller:
             {
                 id = "",
@@ -100,43 +75,17 @@ export default {
     },
 
     async getProductById(id, keySeller) {
-
-        const data = {
-            data: {
-                id: ID,
-                name: "Nevera",
-                brand: "Oster",
-                thubnail: "URL",
-                pictures: ['asdads', 'asdasads'],
-                city:
-                {
-                    name: "Medellin",
-                    code: "MED"
-                },
-                seller:
-                {
-                    id = "",
-                    name = "Name",
-                    logo = 'asdaasd'
-                },
-                description: "",
-                price: 0.0,
-                currency: "COP",
-                rating: 0.0
-            }
-        };
-
         switch (keySeller) {
             case 0:
-                //const data = await api.api1().get(`api/item/:${id}`)
+                const data = await api.api1().get(`api/item/:${id}`)
                 return data.data;
                 break;
             case 1:
-                //const data = await api.api2().get(`api/item/:${id}`)
+                const data = await api.api2().get(`api/item/:${id}`)
                 return data.data;
                 break;
             case 2:
-                //const data = await api.api3().get(`api/item/:${id}`)
+                const data = await api.api3().get(`api/item/:${id}`)
                 return data.data;
                 break;
             default:
