a718b66c5c8b9eadc8a0045ca1ffc480fe3fe472
Sebastian gomez a <sebas970218@gmail.com>
2020-12-03 19:04:10 -0500

    add functionality of search

--- src/services/product.services.js
+++ src/services/product.services.js
@@ -3,91 +3,102 @@ import api from './api'
 export default {
 
     async getProducts(searchWord) {
-
-        const data1 = await api.api1().get(`api/search?q=${searchWord}`)
-        /*
-        const data2 = await api.api2().get(`api/search?q=${searchWord}`)
-        const data3 = await api.api3().get(`api/search?q=${searchWord}`)*/
+        try {
 
 
-        const data2 = {
-            query = "televisor",
-            total = 1,
-            seller:
-            {
-                id = "",
-                name = "vendedeo 2"
-            },
-            items:
-                [
+            const data1 = await api.api1.get('api/search?q=' + searchWord)
+            /*
+         const data2 = await api.api2().get(`api/search?q=${searchWord}`)
+         const data3 = await api.api3().get(`api/search?q=${searchWord}`)*/
+
+
+            const data2 = {
+                data: {
+                    query: "",
+                    total: 1,
+                    seller:
                     {
-                        id: ID,
-                        name: "Nevera vendedor 2",
-                        brand: "Oster",
-                        thubnail: "URL",
-                        city:
-                        {
-                            name: "Medellin",
-                            code: "MED"
-                        },
-                        price: 0.0,
-                        currency: "COP",
-                        rating: 0.0
+                        id: "",
+                        name: "vendedeo 2"
                     },
+                    items:
+                        [
+                            {
+                                id: 'ID',
+                                name: "Nevera vendedor 2",
+                                brand: "Oster",
+                                thubnail: "URL",
+                                city:
+                                {
+                                    name: "Medellin",
+                                    code: "MED"
+                                },
+                                price: 0.0,
+                                currency: "COP",
+                                rating: 0.0
+                            },
 
-                ]
-        }
+                        ]
+                }
+            }
 
-        const data3 = {
-            query = "televisor",
-            total = 1,
-            seller:
-            {
-                id = "",
-                name = "Name"
-            },
-            items:
-                [
+            const data3 = {
+                daata: {
+                    query: "televisor",
+                    total: 1,
+                    seller:
                     {
-                        id: ID,
-                        name: "moto vendedor  3",
-                        brand: "Oster",
-                        thubnail: "URL",
-                        city:
-                        {
-                            name: "Medellin",
-                            code: "MED"
-                        },
-                        price: 0.0,
-                        currency: "COP",
-                        rating: 0.0
+                        id: "",
+                        name: "Name"
                     },
+                    items:
+                        [
+                            {
+                                id: 'ID',
+                                name: "moto vendedor  3",
+                                brand: "Oster",
+                                thubnail: "URL",
+                                city:
+                                {
+                                    name: "Medellin",
+                                    code: "MED"
+                                },
+                                price: 0.0,
+                                currency: "COP",
+                                rating: 0.0
+                            },
 
-                ]
+                        ]
+                }
+            }
+
+            var products = new Array()
+            products.push(data1.data)
+            products.push(data2.data)
+            products.push(data3.data)
+
+            return products;
+        } catch (error) {
+            console.log(error);
+            return {};
         }
-        var products = new Array()
-        products.push(data1.data)
-        products.push(data2.data)
-        products.push(data3.data)
-
-        return products;
-
     },
 
     async getProductById(id, keySeller) {
+        let results;
         switch (keySeller) {
             case 0:
-                const data = await api.api1().get(`api/item/:${id}`)
-                return data.data;
-                break;
+                results = await api.api1().get(`api/item/:${id}`)
+                return results.data;
+
             case 1:
-                const data = await api.api2().get(`api/item/:${id}`)
-                return data.data;
-                break;
+                results = await api.api2().get(`api/item/:${id}`)
+                return results.data;
+
             case 2:
-                const data = await api.api3().get(`api/item/:${id}`)
-                return data.data;
-                break;
+                results = await api.api3().get(`api/item/:${id}`)
+                return results.data;
+
             default:
                 break;
         }
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <h1>products</h1>
-    <h2>{{this.search}}</h2>
+    <h2>{{ this.search }}</h2>
   </div>
 </template>
 
 <script>
+import productServices from "../services/product.services";
+
 export default {
   name: "Products",
 
   data: () => ({
     search: "",
+    products: [],
   }),
 
   async created() {
     this.search = this.$route.params.search;
+    this.products = await productServices.getProducts(this.search);
   },
 };
 </script>
