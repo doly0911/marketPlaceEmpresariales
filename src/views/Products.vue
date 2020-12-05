59ba2f2fef0b9285150dd8308a089a3de033a2a9
alcalyxz <jposadasepulveda@gmail.com>
2020-12-05 00:12:37 -0500

    Primer Front para listar productos

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,7 +1,112 @@
 <template>
   <div>
-    <h1>products</h1>
-    <h2>{{ this.search }}</h2>
+    <v-row>
+      <div>
+        
+        <v-col id="filtrosMenu">
+          <v-card class="mx-auto" max-width="200">
+            <v-card-title class="space-betwen">
+              Filtros
+            </v-card-title>
+
+            <v-divider></v-divider>
+
+            <v-list flat subheader three-line>
+              <v-subheader>Tipos de Producto</v-subheader>
+
+              <v-list-item-group>
+                <v-list-item>
+                  <template v-slot:default="{ active }">
+                    <v-list-item-action>
+                      <v-checkbox :input-value="active"></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                      <v-list-item-title id="opcionesFiltro">Iphone 11</v-list-item-title>
+                      <v-list-item-subtitle
+                        ></v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </template>
+                </v-list-item>
+
+                <v-list-item>
+                  <template v-slot:default="{ active }">
+                    <v-list-item-action>
+                      <v-checkbox :input-value="active"></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                      <v-list-item-title id="opcionesFiltro">Iphone 11 pro</v-list-item-title>
+                      <v-list-item-subtitle
+                        ></v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </template>
+                </v-list-item>
+
+                <v-list-item>
+                  <template v-slot:default="{ active }">
+                    <v-list-item-action>
+                      <v-checkbox :input-value="active"></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                      <v-list-item-title id="opcionesFiltro">Iphone 11 pro plus</v-list-item-title>
+                      <v-list-item-subtitle
+                        ></v-list-item-subtitle
+                      >
+                    </v-list-item-content>
+                  </template>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
+        </v-col>
+      </div>
+
+      <!--Recorrer el Array de proveedores que son 3 -->
+      <div v-for="(product, index) in products" v-bind:key="index">
+        <div v-if="product != null">
+          <v-col id="items">
+            <!--Recorrer Los items del vendedor -->
+            <v-card
+              class="mx-auto my-12"
+              max-width="265px"
+              v-for= "item in product.items"
+              v-bind:key="item.id"
+            >
+              <v-img
+                v-bind:src="item.thumbnail"
+                v-on:click="goDetails(index, item.id)"
+              ></v-img>
+              <v-card-text>
+                <!--ESTA ES LA CLASE QUE ME CENTRA TODO -->
+                <div class="text-center">
+                  <v-card-title id="productName" class="space-betwen" >{{
+                    item.name
+                  }}</v-card-title>
+                  <v-card-text id="productBrand" class="space-betwen">{{
+                    item.brand
+                  }}</v-card-text>
+                  <v-row>
+                    <v-col>
+                      <p id="oldPrice">{{ item.price }}</p>
+                    </v-col>
+                    <v-col>
+                      <p id="newPrice">{{ item.price }}</p>
+                    </v-col>
+                  </v-row>
+                  <v-btn rounded color="#772CE8" dark v-on:click="addToCart(index, item.id)">
+                    Agregar al carrito
+                  </v-btn>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </div>
+      </div>
+    </v-row>
   </div>
 </template>
 
@@ -22,3 +127,72 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#productBrand {
+  font: var(--unnamed-font-style-bold) bold var(--unnamed-font-weight-600) 12px/18px
+    var(--unnamed-font-family-poppins);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-772ce8);
+  font: bold bold 600 12px;
+  letter-spacing: 0px;
+  color: #772ce8;
+  opacity: 1;
+  font-size: 12px;
+  font-weight: bold;
+}
+#productName {
+  font: var(--unnamed-font-style-normal) normal
+    var(--unnamed-font-weight-medium) var(--unnamed-font-size-13) / 16px
+    var(--unnamed-font-family-poppins);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-3b3b3b);
+  text-align: center;
+  letter-spacing: 0px;
+  color: #3b3b3b;
+  opacity: 1;
+  font-size: 14px;
+}
+#oldPrice {
+  text-decoration: line-through;
+
+  font: var(--unnamed-font-style-normal) normal
+    var(--unnamed-font-weight-medium) var(--unnamed-font-size-13) / 16px
+    var(--unnamed-font-family-poppins);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-3b3b3b);
+  text-align: center;
+  font: normal normal medium 13px/16px Poppins;
+  letter-spacing: 0px;
+  color: #3b3b3b;
+  opacity: 1;
+  font-size: 16px;
+}
+#newPrice {
+  font: var(--unnamed-font-style-normal) normal bold 14px/21px
+    var(--unnamed-font-family-poppins);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-772ce8);
+  text-align: center;
+  letter-spacing: 0px;
+  color: #772ce8;
+  opacity: 1;
+  font-weight: bold;
+  font-size: 16px;
+}
+.v-card {
+  border: 1px solid #e2e2e2;
+  border-radius: 8px;
+  opacity: 1;
+}
+
+#opcionesFiltro{
+  font-size: 12px;
+}
+
+#items {
+  width: 300px;
+  display: inline-block;
+}
+</style>
+
