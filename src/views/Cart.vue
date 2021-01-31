459a96b8e8b49c54ebbdbd5b137aeac02fead4d6
veluaru <angie.castaneda@udea.edu.co>
2021-01-31 17:59:21 -0500

    Carrito funcional

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,7 +1,22 @@
 <template>
   <v-container>
-    <h1>Carrito</h1>
-    {{ this.cart }}
+    <h1 id="TituloCarrito">Carrito de Compras</h1>
+    <v-card id="Card">
+      <v-row v-bind:key="index" v-for="(item,index) in cart" cols="12" sm="3">
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-avatar rounded size="100">
+              <img :src="item.image" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-html="item.name"></v-list-item-title>
+              <v-list-item-subtitle v-html="item.price"></v-list-item-subtitle>
+            </v-list-item-content>
+            <v-btn elevation="2" @click="deleteItem(index)" rounded>Eliminar</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
@@ -11,6 +26,13 @@ export default {
   data: () => ({
     cart: [],
   }),
+  methods: {
+    deleteItem: function(index) {
+      this.cart.splice(index,1);
+      const parsed = JSON.stringify(this.cart);
+      localStorage.setItem("cart", parsed);
+    }
+  },
 
   mounted() {
     if (localStorage.getItem("cart")) {
@@ -26,3 +48,22 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#TituloCarrito {
+  top: 593px;
+  left: 444px;
+  width: 479px;
+  height: 49px;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-772ce8);
+  text-align: left;
+  letter-spacing: 0px;
+  color: #772ce8;
+  opacity: 1;
+}
+#Card {
+  margin: auto;
+  padding-left: 15px;
+}
+</style>
