7de0dfde842858af2c3b34cb01fa456cc3235c49
Doly Jimenez <doly.jimenez11@gmail.com>
2021-05-04 19:11:08 -0500

    deploy

---
+++ precache-manifest.7f50c120599111a45ecabec34597351e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152c6270331885c3425e",
+    "url": "/marketPlaceEmpresariales/css/app.32917ab8.css"
+  },
+  {
+    "revision": "3133edd69195f608b0ba",
+    "url": "/marketPlaceEmpresariales/css/chunk-vendors.0883c44d.css"
+  },
+  {
+    "revision": "70a57c418c9a4a08697094ac97549437",
+    "url": "/marketPlaceEmpresariales/img/carrito-icon.70a57c41.svg"
+  },
+  {
+    "revision": "3d72d268ccf4fe0755a3cc4a5ad31111",
+    "url": "/marketPlaceEmpresariales/img/icon-calidad-garantizada.3d72d268.svg"
+  },
+  {
+    "revision": "66f7f98718c4ce473ecbb6e4b3b38049",
+    "url": "/marketPlaceEmpresariales/img/icon-compras-seguras.66f7f987.svg"
+  },
+  {
+    "revision": "df2691154f1466b1e0ab819b25ae2ede",
+    "url": "/marketPlaceEmpresariales/img/icon-envios-todo-elpais.df269115.svg"
+  },
+  {
+    "revision": "b1a361db3150a346dc9785ac7c49bcb7",
+    "url": "/marketPlaceEmpresariales/img/icon-filter.b1a361db.svg"
+  },
+  {
+    "revision": "506e632c26221114eccb525b1db5f682",
+    "url": "/marketPlaceEmpresariales/img/icon-multiples-medios-pago.506e632c.svg"
+  },
+  {
+    "revision": "55ff83a2432a34c16dd5e31e86d04b88",
+    "url": "/marketPlaceEmpresariales/img/login-icon.55ff83a2.svg"
+  },
+  {
+    "revision": "fdd9e3da102eb04a8806e06458753176",
+    "url": "/marketPlaceEmpresariales/img/logo-oldwave-header.fdd9e3da.png"
+  },
+  {
+    "revision": "e31594fc51b3ddb26dba0e642e293ee3",
+    "url": "/marketPlaceEmpresariales/index.html"
+  },
+  {
+    "revision": "152c6270331885c3425e",
+    "url": "/marketPlaceEmpresariales/js/app.7d2b3236.js"
+  },
+  {
+    "revision": "3133edd69195f608b0ba",
+    "url": "/marketPlaceEmpresariales/js/chunk-vendors.503770db.js"
+  },
+  {
+    "revision": "6d531ad0229cedac7da9380c3ee5022a",
+    "url": "/marketPlaceEmpresariales/logo.png"
+  },
+  {
+    "revision": "8a13523befeaf8ad69bcb9376aaea47d",
+    "url": "/marketPlaceEmpresariales/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/marketPlaceEmpresariales/robots.txt"
+  }
+]);
